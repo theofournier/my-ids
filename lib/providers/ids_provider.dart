03dd7853404ddbecc03869c97f52b18779084ba5
theofournier <fournier.theo@gmail.com>
2020-01-22 13:59:32 -0500

    Update provider Ids

--- lib/providers/ids_provider.dart
+++ lib/providers/ids_provider.dart
@@ -1,18 +1,17 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:my_ids/models/id_model.dart';
 import 'package:my_ids/utils/data_example.dart';
+import 'package:my_ids/utils/utils.dart';
 
 class IdsProvider with ChangeNotifier {
-  List<IdModel> _ids;
+  List<IdModel> _ids = [];
 
   List<IdModel> get ids {
     return [..._ids];
   }
 
   Future<void> fetchIds() async {
-    sleep(Duration(seconds: 2));
+    await Utils.sleep(2);
     _ids = DataExample.ids;
     notifyListeners();
   }
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,2 +1,6 @@
 class Utils {
+  static Future<void> sleep(int seconds){
+    return Future.delayed(Duration(seconds: seconds));
+  }
+
 }
