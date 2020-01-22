cc454161a25efdb207390fdf4aadd1f01586e074
theofournier <fournier.theo@gmail.com>
2020-01-22 12:53:28 -0500

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 final ThemeData themeData = new ThemeData(
   primarySwatch: const MaterialColor(0xFF3C70A4, AppColors.color),
-  primaryColor: const Color(0xFF1d3e53),
-  primaryColorDark: const Color(0xFF00182a),
-  primaryColorLight: const Color(0xFF4a687f),
+  primaryColor: const Color(0xFF2c7873),
+  primaryColorDark: const Color(0xFF004c48),
+  primaryColorLight: const Color(0xFF5ea7a2),
   accentColor: const Color(0xFFffba5a),
   errorColor: const Color(0xFFAF6566),
   fontFamily: 'OpenSans',
@@ -13,16 +13,16 @@ final ThemeData themeData = new ThemeData(
 class AppColors {
   AppColors._(); // this basically makes it so you can instantiate this class
   static const Map<int, Color> color = const <int, Color>{
-    50: const Color(0x1A1d3e53),
-    100: const Color(0x331d3e53),
-    200: const Color(0x4D1d3e53),
-    300: const Color(0x661d3e53),
-    400: const Color(0x801d3e53),
-    500: const Color(0x991d3e53),
-    600: const Color(0xB31d3e53),
-    700: const Color(0xCC1d3e53),
-    800: const Color(0xE61d3e53),
-    900: const Color(0xFF1d3e53)
+    50: const Color(0x1A2c7873),
+    100: const Color(0x332c7873),
+    200: const Color(0x4D2c7873),
+    300: const Color(0x662c7873),
+    400: const Color(0x802c7873),
+    500: const Color(0x992c7873),
+    600: const Color(0xB32c7873),
+    700: const Color(0xCC2c7873),
+    800: const Color(0xE62c7873),
+    900: const Color(0xFF2c7873)
   };
 
   static const Color greyBackground = const Color(0xFFF5F5F5);
