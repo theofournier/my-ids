619b6346fee35492b3dae67294f28c81b4121b6f
theofournier <fournier.theo@gmail.com>
2020-01-22 11:50:50 -0500

    Add route file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:my_ids/theme.dart' as Theme;
+import 'package:my_ids/routes.dart' as Routes;
 
 import 'generated/i18n.dart';
 
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
       supportedLocales: S.delegate.supportedLocales,
       theme: Theme.themeData,
       onGenerateTitle: (ctx) => S.of(ctx).appTitle,
+      routes: Routes.routes,
       home: Center(child: Text(S.of(context).appTitle),),
     );
   }
